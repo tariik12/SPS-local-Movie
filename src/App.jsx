7e2884c18e92e19341dc3fbc315241f01fffe840
tarikul-islam <tariikul12@gmail.com>
2023-03-30 13:03:42 +0600

    header section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App bg-black text-white">
-      <p>hello</p>
+    <div className="App bg-black text-white container">
+      <Header />
+
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white fa-bold">Movie World</a>
+    <form class="d-flex" role="search">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        </div>
+    );
+};
+
+export default Header;
